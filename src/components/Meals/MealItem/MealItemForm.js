c4222120e4ad49c47c2b77ed72c3cd14d7a81909
jerry <qaz013039@gmail.com>
2022-06-30 23:11:00 +0800

    Adding a Cart Reducer and Working with Refs & Forward Refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,31 @@
 /** @format */
+import { useRef, useState } from "react";
 
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountValid, setAmountValid] = useState(true);
+
+  const onSubmitHandler = event => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      setAmountValid(false);
+      return;
+    }
+
+    props.addToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -17,7 +36,8 @@ const MealItemForm = (props) => {
           defaultValue: "1",
         }}
       />
-      <button>+ Add</button>
+      <button type="submit">+ Add</button>
+      {!amountValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
